0a713d66a2f57895a777f2e052691e2189344582
Ethan Bruning <ethan@bruning.io>
2023-03-19 15:37:04 -0700

    disable plugins to fix completion

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    enabled = false,
     keys = {
       -- disable the keymap to grep files
-      {"<leader>/", false},
+      { "<leader>/", false },
       -- add a keymap to browse plugin files
       {
         "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
         desc = "Find Plugin File",
       },
     },
