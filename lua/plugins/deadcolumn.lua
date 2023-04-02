f6154cf7c230fa5c5b5c19dd8e8b932bb4674c80
Ethan Bruning <ethan@bruning.io>
2023-04-02 12:29:03 -0700

    fix json

--- lua/plugins/deadcolumn.lua
+++ lua/plugins/deadcolumn.lua
@@ -1,17 +1,22 @@
 return {
-  { "Bekaboo/deadcolumn.nvim" },
-  -- config = function() require("zenburn").setup() end
-  opts = {
-    threshold = 0.75,
-    scope = "line",
-    modes = { "i", "ic", "ix", "R", "Rc", "Rx", "Rv", "Rvc", "Rvx" },
-    warning = {
-      alpha = 0.4,
-      colorcode = "#FF0000",
-      hlgroup = {
-        "Error",
-        "background",
+  {
+    "Bekaboo/deadcolumn.nvim",
+    -- config = true,
+    opts = {
+      threshold = 0.75,
+      scope = "line",
+      modes = { "i", "ic", "ix", "R", "Rc", "Rx", "Rv", "Rvc", "Rvx" },
+      warning = {
+        alpha = 0.4,
+        colorcode = "#FF0000",
+        hlgroup = {
+          "Error",
+          "background",
+        },
       },
     },
+    config = function(_, opts)
+      require("deadcolumn").setup(opts)
+    end,
   },
 }
